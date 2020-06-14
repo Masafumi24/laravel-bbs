39639fc0302a7fe6fcc159444a24988001d40ca6
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-06-14 20:01:04 +0900

    comments OK

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PostsController@index')->name('top');
 Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show']]);
+
+Route::resource('comments', 'CommentsController', ['only' => ['store']]);
